32d4213163dfd734519276e43d1fa3a0bab67e60
carthioli <carthivie@gmail.com>
2022-01-10 17:35:47 -0300

    reserva
    falta inserir reserva livro

--- controle/insere/insereReserva.php
+++ controle/insere/insereReserva.php
@@ -9,10 +9,10 @@
     use Carlos\Biblioteca\Mensagem\Mensagem;      
 
     if( isset( $_POST['id_livros'] ) ){
-      $id_livro = $_POST['id_livros'];
+      $id_livro[] = $_POST['id_livros'];
     }
     if( isset( $_POST['id_livro'] ) ){
-      $id_livro = $_POST['id_livro'];
+      $id_livro[] = $_POST['id_livro'];
     }
 
     if ( !empty( $_POST['id_aluno'] ) &&
@@ -20,21 +20,20 @@
                 is_numeric( $_POST['id_aluno'] ) ) {
         
        $id_aluno = $_POST['id_aluno'];           
-
+       
         $reserva = (new Reserva)->inserirReserva($id_aluno);
-        echo json_encode(array( 'id_livro' => $id_livro, 'erro' => false));
+        
         if( $reserva == true ){
-
-          $ultimoId = (new Conexao)->ultimoId();
           
-         /* foreach($_POST['id_livro'] as $livro){
-            
-            $emprestimoLivro = (new Emprestimo)->inserirEmprestimoLivro($livro, $ultimoEmprestimo['id'], $_POST['dias_devolucao'], $ultimoEmprestimo['data_emprestimo']);
-             
-          } */
+          $ultimoId = (new Conexao)->ultimoId( 'reserva' );
+          echo json_encode(array('id_livro' => $livro, 'erro' => false));
+          foreach($id_livro AS $livro){
+             // $reservaLivro = (new Reserva)->inserirReservaLivros( $livro, $ultimoId['id'] );
+              
+          }
+       
           
-         
-
+          
         }
     }else{
       echo json_encode(array('livro' => $id_livro, 'erro' => true));
--- src/App/Reserva.php
+++ src/App/Reserva.php
@@ -19,8 +19,17 @@
           return true;
         }
       }
-      function inserirReservaLivro($id_livro){
-      
+      function inserirReservaLivro( $id_aluno ){
+        $link = new Conexao;
+
+        $inserir = "INSERT INTO reserva(id_aluno) VALUES ($id_aluno)";
+        $inseriu = pg_query( $link->conecta(), $inserir ); 
+          //$inserir = "INSERT INTO reserva_livro(id_livro, id_reserva) VALUES ($id_livro, $id_reserva)";
+          //$inseriu = pg_query( $link->conecta(), $inserir ); 
+
+          if( pg_affected_rows( $inseriu ) ){
+            return true;
+          }
       }
     }
 ?>
